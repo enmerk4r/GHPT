df342cb6a8eb93d11d193446943f6fb8fb6c48fd
clicketyclackety <csykes@structurecraft.com>
2023-06-24 16:57:05 -0700

    Added fuzzy pair fixes for certain objects

--- GHPT/Utils/GraphUtil.cs
+++ GHPT/Utils/GraphUtil.cs
@@ -1,26 +1,24 @@
 ﻿using Grasshopper;
 using Grasshopper.Kernel;
-using Grasshopper.Kernel.Data;
-using Grasshopper.Kernel.Types;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
-using System.Collections;
 
 namespace GHPT.Utils
 {
     public static class GraphUtil
     {
+
+        private static readonly Dictionary<string, string> fuzzyPairs = new()
+        {
+            { "Extrusion", "Extrude" },
+        };
+
+
         public static void InstantiateComponent(GH_Document doc, string name, System.Drawing.PointF pivot)
         {
             try
             {
-                IGH_ObjectProxy myProxy = Instances.ComponentServer.FindObjectByName(name, true, true);
+                IGH_ObjectProxy myProxy = GetObject(name);
 
                 Guid myId = myProxy.Guid;
                 GH_Component myComponent = (GH_Component)Instances.ComponentServer.EmitObject(myId);
@@ -32,6 +30,20 @@ namespace GHPT.Utils
             catch
             {
             }
+        }
+
+        private static IGH_ObjectProxy GetObject(string name)
+        {
+            IGH_ObjectProxy myProxy = Instances.ComponentServer.FindObjectByName(name, true, true);
+            if (myProxy is null)
+            {
+                if (fuzzyPairs.ContainsKey(name))
+                {
+                    myProxy = Instances.ComponentServer.FindObjectByName(fuzzyPairs[name], true, true);
+                }
+            }
+
+            return myProxy;
         }
 
     }
