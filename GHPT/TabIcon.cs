bf435a47373452a98a57f263fb084b8d89220ed6
clicketyclackety <csykes@structurecraft.com>
2023-06-25 06:53:52 -0700

    Created/Added branding

---
+++ GHPT/TabIcon.cs
@@ -0,0 +1,9 @@
+﻿public class GHPTCategoryIcon : Grasshopper.Kernel.GH_AssemblyPriority
+{
+    public override Grasshopper.Kernel.GH_LoadingInstruction PriorityLoad()
+    {
+        Grasshopper.Instances.ComponentServer.AddCategoryIcon("GHPT", GHPT.Resources.Icons.dark_logo_24x24);
+        Grasshopper.Instances.ComponentServer.AddCategorySymbolName("GHPT", 'G');
+        return Grasshopper.Kernel.GH_LoadingInstruction.Proceed;
+    }
+}
