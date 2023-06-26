2c4a3a5e97c4862833a34f457c5ed00ddcfa23bf
clicketyclackety <csykes@structurecraft.com>
2023-06-26 12:29:09 -0700

    Added in status code check

--- GHPT/Utils/ClientUtil.cs
+++ GHPT/Utils/ClientUtil.cs
@@ -1,21 +1,18 @@
 ﻿using GHPT.IO;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GHPT.Utils
 {
-    public static  class ClientUtil
+    public static class ClientUtil
     {
-        public static async Task<ResponsePayload> Ask(string prompt, double temperature=0.7)
+        public static async Task<ResponsePayload> Ask(string prompt, double temperature = 0.7)
         {
             var url = "https://api.openai.com/v1/chat/completions";
 
-            AskPayload payload = new AskPayload(
+            AskPayload payload = new(
                 ConfigUtil.GptModel,
                 prompt,
                 temperature);
@@ -24,6 +21,11 @@ namespace GHPT.Utils
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {ConfigUtil.GptToken}");
 
             var response = await client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json"));
+
+            int statusCode = (int)response.StatusCode;
+            if (statusCode < 200 || statusCode >= 200)
+                throw new System.Exception($"Error: {response.StatusCode} {response.ReasonPhrase} {await response.Content.ReadAsStringAsync()}");
+
             var result = await response.Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<ResponsePayload>(result);
