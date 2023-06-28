e95167864121e59631524ae8024d9964349f5a05
Jo Kamm <jokamm@gmail.com>
2023-06-27 21:14:22 -0500

    Fixed Name
    Changed Kam to Kamm in Documentation

--- GHPT/GHPTInfo.cs
+++ GHPT/GHPTInfo.cs
@@ -20,7 +20,7 @@ namespace GHPT
         public override Guid Id => new("eb88b742-c2cf-4efb-8005-4e3729e66881");
 
         /// <inheritdoc/>
-        public override string AuthorName => "Callum Sykes, Jo Kam, Quoc Dang, Ryan Erbert, Sergey Pigach";
+        public override string AuthorName => "Callum Sykes, Jo Kamm, Quoc Dang, Ryan Erbert, Sergey Pigach";
 
         /// <inheritdoc/>
         public override string AuthorContact => "https://github.com/enmerk4r/GHPT";
