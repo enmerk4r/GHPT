97d74f8f9a01952ff3cea9d58b2b271aa6c6f453
Jo Kamm <jokamm@gmail.com>
2023-06-27 21:01:36 -0500

    Change category to Extra
    Move from dedicated Tab to Extra tab

--- GHPT/TabIcon.cs
+++
@@ -1,9 +0,0 @@
-﻿public class GHPTCategoryIcon : Grasshopper.Kernel.GH_AssemblyPriority
-{
-    public override Grasshopper.Kernel.GH_LoadingInstruction PriorityLoad()
-    {
-        Grasshopper.Instances.ComponentServer.AddCategoryIcon("GHPT", GHPT.Resources.Icons.dark_logo_full_24x24);
-        Grasshopper.Instances.ComponentServer.AddCategorySymbolName("GHPT", 'G');
-        return Grasshopper.Kernel.GH_LoadingInstruction.Proceed;
-    }
-}
